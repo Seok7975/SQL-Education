a25b7047a1173bba678a1aca33283213f47a57f0
ParkHongSeok <seok7975@naver.com>
2024-04-11 19:46:45 +0900

    -

--- Day40to49/Day41_240405/1. MySQL 2/1. MySQL 2/sql/webdb.sql
+++
@@ -1,5 +0,0 @@
-insert into goodsinfo values('10001', '뇌를 자극하는 Java 프로그래밍', '김윤명', 27000);
-insert into goodsinfo values('10002', '좋은 사진을 만드는 노출', '정승익', 20000);
-insert into goodsinfo values('10003', '예제로 배우는 엑셀 2007', '김대식', 19000);
-insert into goodsinfo values('10004', '지금 당장 경제공부 시작하라', '최진기', 18500);
-insert into goodsinfo values('10005', 'Introduction to Algorithms', '토마스 코멘', 42000);
